643cafd57c39c653dadd57c9a8b47e7583f58af3
itsprashant1999 <itsprashant1999@gmail.com>
2022-07-09 04:21:16 +0530

    yoo

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import Navbar from './Components/Navbar';
+import Banner from './Components/Banner';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Banner/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+// typed rcc for class component
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div style={{display:'flex' , padding:'0.5' }}>
+        <h1>Movies App</h1>
+        <h2 style={{marginLeft:'2rem' , marginTop:'1.7rem'}}>Favourites</h2>
+      </div>
+    )
+  }
+}
