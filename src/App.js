ebed2e96b3ea471f44d79de91f32ee32c85d0c2d
itsprashant1999 <itsprashant1999@gmail.com>
2022-09-13 04:00:04 +0530

    complete, few instructions par needed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import logo from './logo.svg';
+import './App.css';
 import Navbar from './Components/Navbar';
 import Banner from './Components/Banner';
-import './App.css';
-
+import List from './Components/List';
+import Favourites from './Components/Favourites';
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
 function App() {
   return (
-    <>
-    <Navbar/>
-    <Banner/>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Banner />
+              <List />
+            </>
+          }
+        />
+        <Route path="/fav" element={<Favourites />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,26 @@
-// typed rcc for class component
 import React, { Component } from 'react'
-
+import { Link } from "react-router-dom";
 export default class Navbar extends Component {
   render() {
     return (
-      <div style={{display:'flex' , padding:'0.5' }}>
-        <h1>Movies App</h1>
-        <h2 style={{marginLeft:'2rem' , marginTop:'1.7rem'}}>Favourites</h2>
+      <div
+        style={{
+          display: "flex",
+          background: "white",
+          color: "blue",
+          padding: "1rem",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <h1>Movies App</h1>
+        </Link>
+
+        <Link to="/fav" style={{ textDecoration: "none" }}>
+          <h2 style={{ marginLeft: "2rem" }}>Favourites</h2>
+        </Link>
       </div>
-    )
+    );
   }
 }
